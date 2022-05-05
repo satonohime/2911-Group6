f404376a3ffb781770b6fe4b481b57e7b6618b46
MSI <ksidhukhushpreet@gmail.com>
2022-05-05 16:30:45 -0700

    modified and refined webapp for index.html

--- webapp.py
+++ webapp.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask import Flask, render_template
 from models.media import Media
 from models.mediamanager import MediaManager
@@ -15,19 +14,17 @@ PRESET_TYPE_DATA = [
 
 @app.route("/")
 def homepage():
-    pass
     mediamnger = MediaManager()
     media_entries = mediamnger.media
-    return render_template('index.html', data=media_entries, types=PRESET_TYPE_DATA)
+    return render_template('index.html', data=media_entries, types=mediamnger.types, manager=mediamnger), 200
 
 @app.route("/<string:media_type>/<string:name>", methods=["GET"])
 def get_entry(media_type, name):
     mediamnger = MediaManager()
     entry = mediamnger.view_media(name, media_type)
-    print(entry.field_1)
     if entry is None:
         return 'Entry not found', 404
-    return render_template('entry.html', name=name, data=entry.to_dict())
+    return render_template('entry.html', name=name, data=entry.to_dict()), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
