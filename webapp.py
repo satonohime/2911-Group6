6bf5d158a775bf0447d858b678fe9bcbb28f12a5
satonohime <nananariru@gmail.com>
2022-05-11 22:59:16 -0700

    link Flask to new HTML pages, add js scripts for HTML pages, routes complete

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from models.media import Media
 from models.mediamanager import MediaManager
 from models.mediatype import MediaType
@@ -36,32 +36,41 @@ def homepage_updated():
             setattr(media, attribute, request.form[attribute])
         mediamnger.save()
 
-    
     media_entries = mediamnger.media
     return render_template('index.html', data=media_entries, types=mediamnger.types, manager=mediamnger), 200
+
+@app.route("/editentry/<string:media_type>/<string:name>", methods=['PUT'])
+def edit_entry(media_type, name):
+    mediamnger = MediaManager()
+    entry_to_edit = mediamnger.view_media(name, media_type)
+    
+    entry_to_edit.name = request.form['name']
+    entry_to_edit.field_1 = request.form['field_1']
+    entry_to_edit.field_2 =  request.form['field_2']
+    entry_to_edit.field_3 = request.form['field_3']
+    mediamnger.save()
+    return "", 204
 
 @app.route('/addentry', methods=['GET', 'POST'])
 def form_display():
     mediamnger = MediaManager()
     select_media = request.form['medias']
     media_type = mediamnger.search_for_type(select_media)
-    print(media_type)
-    return render_template('addentry.html', type=media_type)
+    return render_template('create.html', type=media_type), 200
 
 @app.route("/editentry/<string:media_type>/<string:name>", methods=['GET'])
 def edit_form_display(media_type, name):
     mediamnger = MediaManager()
     entry = mediamnger.view_media(name, media_type)
-    return render_template('editentry.html', type=media_type, data=entry)
+    return render_template('edit.html', type=media_type, data=entry), 200
+
     
 @app.route("/deleteentry/<string:media_type>/<string:name>", methods=['DELETE'])
 def delete_entry(media_type, name):
     mediamnger = MediaManager()
     mediamnger.delete_media(name, media_type)
     mediamnger.save()
-    media_entries = mediamnger.media
-    return render_template('index.html', data=media_entries, types=mediamnger.types, manager=mediamnger), 200
-
+    return "", 201
 
 @app.route("/<string:media_type>/<string:name>", methods=["GET"])
 def get_entry(media_type, name):
@@ -70,10 +79,6 @@ def get_entry(media_type, name):
     if entry is None:
         return 'Entry not found', 404
     return render_template('entry.html', name=name, data=entry.to_dict()), 200
-
-@app.route("/edit", methods=["GET"])
-def edit():
-    return render_template("edit.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
